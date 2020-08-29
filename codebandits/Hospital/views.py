69dc1afe65de23e1618a7447285e01328243df4e
Vishal <vishalgulati2k@gmail.com>
2020-08-29 22:38:44 +0530

    Search Works

--- codebandits/Hospital/views.py
+++ codebandits/Hospital/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from .models import State,City,Hospital
 # Create your views here.
 def Home(request):
-    hospitallist=list(Hospital.objects.all())
-    return render(request,'index.html',{'hl':hospitallist})
+    if(request.method=='POST'):
+        citylist=list(City.objects.all())
+        hospitallist=list(Hospital.objects.all())
+        hosp=[]
+        for c in hospitallist:
+            n=c.City.Name
+            print(n)
+            if request.POST.get("city")==n:
+                hosp.append(c)
+        return render(request,'index.html',{'hl':hosp,'cl':citylist})
+    else:       
+        hospitallist=list(Hospital.objects.all())
+        citylist=list(City.objects.all())
+        return render(request,'index.html',{'hl':hospitallist,'cl':citylist})
