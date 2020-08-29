411e96290b6c47d6e82eb46342b58483c7d3bda2
Vishal <vishalgulati2k@gmail.com>
2020-08-29 11:23:29 +0530

    Model Working

--- codebandits/Hospital/admin.py
+++ codebandits/Hospital/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import State,City,medicine,Hospital
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(medicine)
+admin.site.register(Hospital)
+
+
+
