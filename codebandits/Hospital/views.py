30c97725dabdc2490c32b2c077c5d93c55326e16
Vishal <vishalgulati2k@gmail.com>
2020-08-30 00:21:33 +0530

    Updated

--- codebandits/Hospital/views.py
+++ codebandits/Hospital/views.py
@@ -15,3 +15,21 @@ def Home(request):
         hospitallist=list(Hospital.objects.all())
         citylist=list(City.objects.all())
         return render(request,'index.html',{'hl':hospitallist,'cl':citylist})
+
+def Index(request):
+    if(request.method=='POST'):
+        user=request.user
+        citylist=list(City.objects.all())
+        hospitallist=list(Hospital.objects.all())
+        hosp=[]
+        for c in hospitallist:
+            n=c.City.Name
+            if request.POST.get("city")==n:
+                hosp.append(c)
+        return render(request,'hospital.html',{'hl':hospitallist,'b':user,'cl':citylist})
+    else:
+        user=request.user
+        hospitallist=list(Hospital.objects.all())
+        citylist=list(City.objects.all())
+        return render(request,'hospital.html',{'hl':hospitallist,'b':user,'cl':citylist})
+    
