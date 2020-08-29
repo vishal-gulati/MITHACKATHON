d591e5f4aca760a8baea1eeafc83c8bd6b82b392
Vishal <vishalgulati2k@gmail.com>
2020-08-29 16:38:21 +0530

    Models Updated

--- codebandits/Hospital/models.py
+++ codebandits/Hospital/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 class State(models.Model):
     Name=models.CharField(max_length=100,unique=True)
+    def __str__(self):
+        return '%s' % (self.Name)
 class City(models.Model):
     Name=models.CharField(max_length=100,unique=True)
     State=models.ForeignKey(State, on_delete=models.CASCADE)
+    def __str__(self):
+        return '%s' % (self.Name)
 class medicine(models.Model):
     Remdisivir=models.IntegerField()
     FabiFlu=models.IntegerField()
+    def __str__(self):
+        return '%s' % ('Medicine')
 class Hospital(models.Model):
     Name=models.CharField(max_length=100,unique=True)
     City=models.ForeignKey(City, on_delete=models.CASCADE)
@@ -22,3 +28,5 @@ class Hospital(models.Model):
     med=models.ForeignKey(medicine,on_delete=models.CASCADE)
     Mob=models.CharField(max_length=12)
     email=models.EmailField(max_length=254)
+    def __str__(self):
+        return '%s' % (self.Name)
