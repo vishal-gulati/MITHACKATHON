c51eac4be8191921619821e976bbb275374734cd
Vishal <vishalgulati2k@gmail.com>
2020-08-29 15:29:32 +0530

    View

--- codebandits/Hospital/views.py
+++ codebandits/Hospital/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import State,City,medicine,Hospital
 # Create your views here.
+def Home(request):
+    hospitallist=list(Hospital.objects.all())
+    return render(request,'home.html',{'hl':hospitallist})
