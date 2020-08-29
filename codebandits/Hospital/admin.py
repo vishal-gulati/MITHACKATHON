f0edb0af546190efd87f04522011e64c5e88ea02
Vishal Gulati <42508968+vishal-gulati@users.noreply.github.com>
2020-08-29 16:55:00 +0530

    Updated

--- codebandits/Hospital/admin.py
+++ codebandits/Hospital/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import State,City,medicine,Hospital
+from .models import State,City,Hospital
 admin.site.register(State)
 admin.site.register(City)
-admin.site.register(medicine)
 admin.site.register(Hospital)
 
 
